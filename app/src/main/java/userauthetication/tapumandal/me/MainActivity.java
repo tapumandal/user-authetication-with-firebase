e7e4a0317dac05ef7a7ca488f5ecb74856403fe7
tapumandal <online.tapu@gmail.com>
2019-05-14 10:32:29 +0600

    Working on signup

--- app/src/main/java/userauthetication/tapumandal/me/MainActivity.java
+++ app/src/main/java/userauthetication/tapumandal/me/MainActivity.java
@@ -2,12 +2,32 @@ package userauthetication.tapumandal.me;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView textView;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.tv_not_registered_user).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.tv_not_registered_user:
+                Toast.makeText(getApplicationContext(), "Clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
