2046ab24720eb70d917ee3503e4dd24d300f76f6
online.tapu@gmail.com <online.tapu@gmail.com>
2019-05-18 04:51:05 +0600

    Signup

--- app/src/main/java/userauthetication/tapumandal/me/activity/SignupActivity.java
+++ app/src/main/java/userauthetication/tapumandal/me/activity/SignupActivity.java
@@ -1,6 +1,16 @@
 package userauthetication.tapumandal.me.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import userauthetication.tapumandal.me.R;
 
 public class SignupActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+    }
 }
